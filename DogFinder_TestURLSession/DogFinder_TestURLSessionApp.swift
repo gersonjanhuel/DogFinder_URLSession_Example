1eabf326a3d8b1f8f94eb2a3202e03e9e074d02a
Gerson Janhuel <gersonjanhuel@gmail.com>
2024-05-07 14:13:54 +0700

    Initial Commit

---
+++ DogFinder_TestURLSession/DogFinder_TestURLSessionApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogFinder_TestURLSessionApp.swift
+//  DogFinder_TestURLSession
+//
+//  Created by Gerson Janhuel on 07/05/24.
+//
+
+import SwiftUI
+
+@main
+struct DogFinder_TestURLSessionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
